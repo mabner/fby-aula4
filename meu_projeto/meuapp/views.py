b8d66abe3888ac5ded879f77c7fca3b1ed1de076
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-01 00:04:23 -0300

    base code from pdf

--- meu_projeto/meuapp/views.py
+++ meu_projeto/meuapp/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.template import loader
 from rest_framework import viewsets
 from .serializers import *
 from .models.pessoa import Pessoa
@@ -5,25 +7,35 @@ from .models import *
 
 
 class PessoaViewSet(viewsets.ModelViewSet):
-	queryset = Pessoa.objects.all().order_by('nome')
-	serializer_class = PessoaSerializer
+    queryset = Pessoa.objects.all().order_by('nome')
+    serializer_class = PessoaSerializer
 
 
 class DepartamentoViewSet(viewsets.ModelViewSet):
-	queryset = Departamento.objects.all()
-	serializer_class = DepartamentoSerializer
+    queryset = Departamento.objects.all()
+    serializer_class = DepartamentoSerializer
 
 
 class TelefoneViewSet(viewsets.ModelViewSet):
-	queryset = Telefone.objects.all()
-	serializer_class = TelefoneSerializer
+    queryset = Telefone.objects.all()
+    serializer_class = TelefoneSerializer
 
 
 class PlantaoViewSet(viewsets.ModelViewSet):
-	queryset = Plantao.objects.all()
-	serializer_class = PlantaoSerializer
+    queryset = Plantao.objects.all()
+    serializer_class = PlantaoSerializer
 
 
 class CargoViewSet(viewsets.ModelViewSet):
-	queryset = Cargo.objects.all()
-	serializer_class = CargoSerializer
+    queryset = Cargo.objects.all()
+    serializer_class = CargoSerializer
+
+
+def detalharpessoa(request):
+
+    pessoas = Pessoa.objects.order_by('nome')
+    template = loader.get_template('pessoa/detalhar.html')
+    context = {
+        'listapessoas': pessoas,
+    }
+    return HttpResponse(template.render(context, request))
